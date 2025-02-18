60d0599d2297e0a7c52d0d5e162ba7f6cc57789c
NSOOITA <74722406+naoto-sato-ooita@users.noreply.github.com>
2025-02-18 17:15:21 +0900

    Update AnnotationView.swift

--- New Group/AnnotationView.swift
+++ New Group/AnnotationView.swift
@@ -81,4 +81,21 @@ struct AnnotationView: View {
     }
 }
 
+struct DropPin: Shape {
+  var startAngle: Angle = .degrees(180)
+  var endAngle: Angle = .degrees(0)
+
+  func path(in rect: CGRect) -> Path {
+    var path = Path()
+    path.move(to: CGPoint(x: rect.midX, y: rect.maxY))
+    path.addCurve(to: CGPoint(x: rect.minX, y: rect.midY),
+                              control1: CGPoint(x: rect.midX, y: rect.maxY),
+                              control2: CGPoint(x: rect.minX, y: rect.midY + rect.height / 4))
+    path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+    path.addCurve(to: CGPoint(x: rect.midX, y: rect.maxY),
+                              control1: CGPoint(x: rect.maxX, y: rect.midY + rect.height / 4),
+                              control2: CGPoint(x: rect.midX, y: rect.maxY))
+    return path
+  }
+}
 
